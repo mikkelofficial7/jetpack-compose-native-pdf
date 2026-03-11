12688e8774dd149a4a877df86c38a76f1bcd04ce
Mikkel <mikkelseptiano86@gmail.com>
2026-03-11 15:29:36 +0700

    move provider to module manifest

--- app/src/main/java/com/jetpack/compose/nativepdf/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/nativepdf/MainActivity.kt
@@ -5,11 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
