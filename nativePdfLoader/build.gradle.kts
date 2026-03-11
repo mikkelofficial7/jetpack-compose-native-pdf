05caad56a979ba81ce1f3c2af7b5479a558c2847
Mikkel <mikkelseptiano86@gmail.com>
2026-03-11 15:41:12 +0700

    build setup for jitpack library

--- nativePdfLoader/build.gradle.kts
+++ nativePdfLoader/build.gradle.kts
@@ -1,17 +1,17 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.compose)
+    id("maven-publish")
 }
 
 android {
     namespace = "com.jetpack.compose.pdfloader"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -25,12 +25,40 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
     buildFeatures {
         compose = true
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                groupId = "com.github.mikkelofficial7"
+                artifactId = "JetpackComposeNativePdf"
+                version = "1.0.0"
+
+                from(components["release"])
+            }
+        }
     }
 }
 
